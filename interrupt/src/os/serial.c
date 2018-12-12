218bdc33881d404972e80f3c59263cac449e3701
car4ryu <car.ryu.ryu.ryu@outlook.com>
2018-12-12 17:05:49 +0900

    [fix]fixed typo in serial.c

--- interrupt/src/os/serial.c
+++ interrupt/src/os/serial.c
@@ -118,7 +118,7 @@ void serial_intr_send_enable(int index){
 
 void serial_intr_send_disable(int index){
 	volatile struct h8_3069f_sci * sci = regs[index].sci;
-	sci->sct &= H8_3069F_SCI_SCR_TIE;
+	sci->scr &= H8_3069F_SCI_SCR_TIE;
 }
 
 int serial_intr_is_recv_enable(int index){
@@ -133,6 +133,6 @@ void serial_intr_recv_enable(int index){
 
 void serial_intr_recv_disable(int index){
 	volatile struct h8_3069f_sci * sci = regs[index].sci;
-	sci->sct &= H8_3069F_SCI_SCR_RIE;
+	sci->scr &= H8_3069F_SCI_SCR_RIE;
 }
 
