caf84540bce0b8cd2f2eb9c62adbef05493cfe25
car4ryu <car.ryu.ryu.ryu@outlook.com>
2018-12-24 02:12:22 +0900

    [add]added memory management system, and now working

--- memory/src/os/kozos.c
+++ memory/src/os/kozos.c
@@ -3,6 +3,7 @@
 #include "intr.h"
 #include "interrupt.h"
 #include "syscall.h"
+#include "memory.h"
 #include "lib.h"
 
 #define THREAD_NUM 6
@@ -210,6 +211,17 @@ static int thread_chpri(int priority){
     return old;
 }
 
+static void *thread_kmalloc(int size){
+	putcurrent();
+	return kzmem_alloc(size);
+}
+
+static int thread_kmfree(char *p){
+	kzmem_free(p);
+	putcurrent();
+	return 0;
+}
+
 /* 割り込みハンドラへの登録 */
 static int setintr(softvec_type_t type, kz_handler_t handler){
     static void thread_intr(softvec_type_t type, unsigned long sp);
@@ -244,6 +256,12 @@ static void call_functions(kz_syscall_type_t type, kz_syscall_param_t *p){
         case KZ_SYSCALL_TYPE_CHPRI:
             p->un.chpri.ret = thread_chpri(p->un.chpri.priority);
             break;
+		case KZ_SYSCALL_TYPE_KMALLOC:
+			p->un.kmalloc.ret = thread_kmalloc(p->un.kmalloc.size);
+			break;
+		case KZ_SYSCALL_TYPE_KMFREE:
+			p->un.kmfree.ret = thread_kmfree(p->un.kmfree.p);
+			break;
         default:
             break;
     }
@@ -296,6 +314,8 @@ static void thread_intr(softvec_type_t type, unsigned long sp){
 /* 初期スレッドのスタート */
 /* これがなきゃ始まらない!!!!! */
 void kz_start(kz_func_t func, char *name, int priority, int stacksize, int argc, char *argv[]){
+	kzmem_init();
+
     current = NULL;
     
     memset(readyque, 0, sizeof(readyque));
@@ -323,6 +343,6 @@ void kz_sysdown(void){
 void kz_syscall(kz_syscall_type_t type, kz_syscall_param_t *param){
     current->syscall.type = type;
     current->syscall.param = param;
-    puts("trapping\n");
     asm volatile("trapa #0"); /* 割り込み発行 */
 }
+
